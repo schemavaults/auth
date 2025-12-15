7dae2b9abd3635dd77996cb3a662260a77ee458c
jalexw <jalexwhitman@gmail.com>
2025-12-15 18:03:31 -0400

    fix resolving monorepo root for auto server build process

--- auth-server/next.config.ts
+++ auth-server/next.config.ts
@@ -1,7 +1,7 @@
 // next.config.ts
 import type { NextConfig } from "next";
 import { join, normalize } from "path";
-import { existsSync } from "fs";
+import { existsSync, readFileSync } from "fs";
 
 const output =
   typeof process.env.NEXT_STANDALONE_DOCKER_BUILD === "string" &&
@@ -9,13 +9,38 @@ const output =
     ? "standalone"
     : undefined;
 
+function resolveAuthServerRootDir(): string {
+  let currentDirectory: string = __dirname;
+  function containsAuthServerPackageJson(dir: string): boolean {
+    if (!existsSync(join(dir, "package.json"))) {
+      return false;
+    }
+
+    const file = readFileSync(join(dir, "package.json"), "utf8");
+    const packageJson = JSON.parse(file);
+    return packageJson.name === "@schemavaults/auth-server";
+  }
+
+  const MAX_TRAVERSE_UPWARDS_ATTEMPTS: number = 8;
+  for (let i = 0; i < MAX_TRAVERSE_UPWARDS_ATTEMPTS; i++) {
+    if (containsAuthServerPackageJson(currentDirectory)) {
+      return normalize(currentDirectory);
+    }
+    currentDirectory = join(currentDirectory, "..");
+  }
+  console.error(
+    "[next.config.ts] Could not find @schemavaults/auth-server package root",
+  );
+  process.exit(1);
+}
+
 console.log(
   `[next.config.ts] Configuring @schemavaults/auth-server... (output mode: ${output ?? "<default>"})`,
 );
 
-const projectRoot = __dirname;
+const projectRoot: string = resolveAuthServerRootDir();
 if (!existsSync(join(projectRoot, "package.json"))) {
-  throw new Error("package.json not found");
+  throw new Error("@schemavaults/auth-server package.json not found");
 }
 const monorepoRoot: string = normalize(join(projectRoot, ".."));
 
