91a1bead2a60dac7933ebfbabe6cb37d992bc25c
jalexw <jalexwhitman@gmail.com>
2025-12-15 17:51:38 -0400

    0.12.10 - explicitly set turborepo root

--- auth-server/next.config.ts
+++ auth-server/next.config.ts
@@ -1,5 +1,7 @@
 // next.config.ts
 import type { NextConfig } from "next";
+import { join, normalize } from "path";
+import { existsSync } from "fs";
 
 const output =
   typeof process.env.NEXT_STANDALONE_DOCKER_BUILD === "string" &&
@@ -11,10 +13,19 @@ console.log(
   `[next.config.ts] Configuring @schemavaults/auth-server... (output mode: ${output ?? "<default>"})`,
 );
 
+const projectRoot = __dirname;
+if (!existsSync(join(projectRoot, "package.json"))) {
+  throw new Error("package.json not found");
+}
+const monorepoRoot: string = normalize(join(projectRoot, ".."));
+
 const nextConfig: NextConfig = {
   // Generate for a standalone container server if NEXT_STANDALONE_DOCKER_BUILD
   output,
   reactStrictMode: true,
+  turbopack: {
+    root: monorepoRoot,
+  },
 };
 
 export default nextConfig;
