f630b389059af158b4cc18d09fdfd5043b8f9ea3
jalexw <jalexwhitman@gmail.com>
2025-12-18 13:52:56 -0400

    @schemavaults/auth-common:0.7.12 - added optional userOrgs array field of organization ids

--- packages/auth-common/src/request_tokens_result.ts
+++ packages/auth-common/src/request_tokens_result.ts
@@ -2,21 +2,31 @@ import { z } from "zod";
 import { userDataSchema } from "./user_data";
 import { accessTokenDataSchema, refreshTokenDataSchema } from "./token-data";
 import { audienceRefSchema } from "./audience-schema";
+import { organizationIdSchema } from "./organizations";
 
-export const requestTokensResultSchema = z.object({
-  success: z.boolean(),
-  message: z.string(),
-  tokens: z.object({
-    access: z.record( // map of audience (app id, fs region, or auth server url) to token for that audience
-      audienceRefSchema,
-      accessTokenDataSchema
-    ).optional(),
-    refresh: refreshTokenDataSchema.optional()
-  }).optional(),
-  userData: userDataSchema.optional()
-}).required({
-  success: true,
-  message: true
-}).strict();
+export const requestTokensResultSchema = z
+  .object({
+    success: z.boolean(),
+    message: z.string(),
+    tokens: z
+      .object({
+        access: z
+          .record(
+            // map of audience (app id, fs region, or auth server url) to token for that audience
+            audienceRefSchema,
+            accessTokenDataSchema,
+          )
+          .optional(),
+        refresh: refreshTokenDataSchema.optional(),
+      })
+      .optional(),
+    userData: userDataSchema.optional(),
+    userOrgs: organizationIdSchema.array().optional(),
+  })
+  .required({
+    success: true,
+    message: true,
+  })
+  .strict();
 
 export type RequestTokensResult = z.infer<typeof requestTokensResultSchema>;
