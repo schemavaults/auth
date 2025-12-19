4f8587584d7a8d648f33f3e8650b729447c5a4df
jalexw <jalexwhitman@gmail.com>
2025-12-19 11:12:21 -0400

    auto-create schemavaults-auth-server-dev database instead of using postgres admin account

--- auth-postgres-db/docker-entrypoint-initdb.d/init-db.sql
+++ auth-postgres-db/docker-entrypoint-initdb.d/init-db.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE "schemavaults-auth-server-dev";
+CREATE USER schemavaults-auth-server-dev WITH ENCRYPTED PASSWORD 'schemavaults-auth-server-dev';
 GRANT ALL PRIVILEGES ON DATABASE "schemavaults-auth-server-dev" TO "schemavaults-auth-server-dev"
