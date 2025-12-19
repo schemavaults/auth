ec627b799d6a5c04e5229e3ba472c58da97d6711
jalexw <jalexwhitman@gmail.com>
2025-12-19 15:25:29 -0400

    @schemavaults/jwt:0.6.5 - explicit tests on expected privacy levels of different keys

--- packages/jwt/src/tests/JsonSerializedJwtKey-Export.test.ts
+++ packages/jwt/src/tests/JsonSerializedJwtKey-Export.test.ts
@@ -36,4 +36,47 @@ describe("JsonSerializedJwtKey-Export", () => {
     expect(isValidJsonSerializedJwtKey(encryption)).toBe(true);
     expect(isValidJsonSerializedJwtKey(decryption)).toBe(true);
   });
+
+  test("decryption key is exported as a private key", async () => {
+    const jwt_keys = await generateNewJwtKeySet({
+      audience_id: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
+    });
+    const decryption: JsonSerializedJwtKey = jwt_keys.decryption_key_json;
+    expect(decryption.privacy_level).toBe("private");
+  });
+
+  test("encryption key is exported as a public key", async () => {
+    const jwt_keys = await generateNewJwtKeySet({
+      audience_id: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
+    });
+    const encryption: JsonSerializedJwtKey | null =
+      jwt_keys.encryption_key_json;
+    if (!encryption) {
+      throw new Error("Encryption key is missing from generated keyset");
+    }
+    expect(encryption.privacy_level).toBe("public");
+  });
+
+  test("signing key is exported as a private key", async () => {
+    const jwt_keys = await generateNewJwtKeySet({
+      audience_id: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
+    });
+    const signing: JsonSerializedJwtKey | null = jwt_keys.signing_key_json;
+    if (!signing) {
+      throw new Error("Signing key is missing from generated keyset");
+    }
+    expect(signing.privacy_level).toBe("private");
+  });
+
+  test("verification key is exported as a public key", async () => {
+    const jwt_keys = await generateNewJwtKeySet({
+      audience_id: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
+    });
+    const verification: JsonSerializedJwtKey | null =
+      jwt_keys.verification_key_json;
+    if (!verification) {
+      throw new Error("Verification key is missing from generated keyset");
+    }
+    expect(verification.privacy_level).toBe("public");
+  });
 });
