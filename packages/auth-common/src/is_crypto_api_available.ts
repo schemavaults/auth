15ab57d8e72641dd2d6964b9c4435a950139b4b6
jalexw <jalexwhitman@gmail.com>
2025-12-21 20:20:57 -0400

    @schemavaults/auth-common:0.7.14 - fix eslint errors for isCryptoApiAvailable

--- packages/auth-common/src/is_crypto_api_available.ts
+++ packages/auth-common/src/is_crypto_api_available.ts
@@ -1,30 +1,30 @@
 export default function isCryptoApiAvailable(): boolean {
-  let isCryptoAvailable: boolean = false;
   // Check if this is a server-side environment-- assume crypto is available then
   try {
     if (!window || typeof window === "undefined") {
-      isCryptoAvailable = true;
+      return true;
     }
   } catch (e: unknown) {
-    isCryptoAvailable = true;
+    void e; // an error means 'window' is not defined and threw-- this is a server
+    return true;
   }
 
   // Check if the browser supports the Web Crypto API
   try {
     if (window && typeof window.crypto !== "undefined") {
       if (window.location.protocol === "https:") {
-        isCryptoAvailable = true;
+        return true;
       }
       if (window.location.href.startsWith("https://")) {
-        isCryptoAvailable = true;
+        return true;
       }
       if (/^http:\/\/(localhost|127\.0\.0\.1)/.test(window.location.href)) {
-        isCryptoAvailable = true;
-      } else {
-        isCryptoAvailable = false;
+        return true;
       }
     }
-  } catch (e: unknown) {}
+  } catch (e: unknown) {
+    void e;
+  }
 
-  return isCryptoAvailable;
+  return false;
 }
