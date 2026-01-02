d71d7650a0d20eb274479050ba3121a509833433
jalexw <jalexwhitman@gmail.com>
2026-01-02 10:46:59 -0400

    include expected SCHEMAVAULTS_APP_ENVIRONMENT in test name

--- tests/e2e-auth-tests/cypress/e2e/AppEnvironmentEndpoint.cy.ts
+++ tests/e2e-auth-tests/cypress/e2e/AppEnvironmentEndpoint.cy.ts
@@ -18,14 +18,26 @@ function loadEnvironmentFromEndpoint(
   });
 }
 
+const SCHEMAVAULTS_APP_ENVIRONMENT = Cypress.env(
+  "SCHEMAVAULTS_APP_ENVIRONMENT",
+);
+if (
+  !SCHEMAVAULTS_APP_ENVIRONMENT ||
+  typeof SCHEMAVAULTS_APP_ENVIRONMENT !== "string"
+) {
+  throw new Error(
+    "SCHEMAVAULTS_APP_ENVIRONMENT is not defined or not a string",
+  );
+}
+
 describe("App Environment Endpoint", () => {
   it("should return a valid environment type", () => {
     loadEnvironmentFromEndpoint();
   });
 
-  it("should match the Cypress environment's SCHEMAVAULTS_APP_ENVIRONMENT value", () => {
+  it(`should match the Cypress environment's SCHEMAVAULTS_APP_ENVIRONMENT value (${SCHEMAVAULTS_APP_ENVIRONMENT})`, () => {
     loadEnvironmentFromEndpoint((environment) => {
-      expect(environment).to.equal(Cypress.env("SCHEMAVAULTS_APP_ENVIRONMENT"));
+      expect(environment).to.equal(SCHEMAVAULTS_APP_ENVIRONMENT);
     });
   });
 });
