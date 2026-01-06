e1f1ad1fd0303c6c4892674b77f7e51e9947bd94
jalexw <jalexwhitman@gmail.com>
2026-01-06 09:07:28 -0400

    making logout test more resilient, wait on login page load

--- tests/e2e-auth-tests/cypress/support/actions/logout.ts
+++ tests/e2e-auth-tests/cypress/support/actions/logout.ts
@@ -7,16 +7,28 @@ export default function logout() {
   cy.getCookie("refresh_token").should("exist");
 
   // Perform logout actions
+  cy.intercept("POST", "**/api/logout").as("logoutRequest");
+  cy.intercept("GET", "**/auth/login?**").as("loginPageLoad");
   cy.get("button#sign-out-button").click();
 
   // Post-logout triggered assertions
-  return cy.wait(3000).then(() => {
+  cy.wait(2000).then(() => {
     cy.url().should("not.include", "/account");
 
-    // Post successful logout assertions
-    return cy.wait(6500).then(() => {
+    cy.wait("@logoutRequest", { timeout: 15000 }).then((interception) => {
+      cy.wrap(interception.response?.statusCode).should("eq", 200);
+      // refresh token should have been cleared by logout request
       cy.getCookie("refresh_token").should("not.exist");
-      cy.url({ timeout: 20000 }).should("not.include", "/auth/logout");
+      cy.log("Logout request appears to have been a success!");
+
+      // Now, make sure that user is sent to login page
+      cy.wait("@loginPageLoad", { timeout: 15000 }).then(() => {
+        // Post successful logout assertions
+        cy.wait(5000).then(() => {
+          cy.url({ timeout: 20000 }).should("not.include", "/auth/logout");
+        });
+      });
     });
+    return;
   });
 }
