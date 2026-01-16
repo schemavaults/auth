2b46bf4fe618bdac129eeff2506de8aee23703bf
jalexw <jalexwhitman@gmail.com>
2026-01-16 16:36:35 -0400

    e2e-auth-tests:0.0.43 - make invite code tests more resilient, fix bug in parsing generated invite code, add wait time before opening invite-code-creation dialog

--- tests/e2e-auth-tests/cypress/e2e/InviteCodes.cy.ts
+++ tests/e2e-auth-tests/cypress/e2e/InviteCodes.cy.ts
@@ -24,19 +24,32 @@ describe("Invite Codes", () => {
       }
 
       const INVITE_CODE_LENGTH: number = 24;
-      cy.generate_random_code(INVITE_CODE_LENGTH).then((invite_code: string) => {
-        if (typeof invite_code !== "string" || invite_code.length === INVITE_CODE_LENGTH) {
-          throw new TypeError("Generated invite code is not a string of the correct length");
-        }
+      cy.generate_random_code(INVITE_CODE_LENGTH).then(
+        (invite_code: string) => {
+          if (typeof invite_code !== "string") {
+            throw new TypeError(
+              `Generated invite code is not a string, received type: '${typeof invite_code}'`,
+            );
+          } else if (invite_code.length !== INVITE_CODE_LENGTH) {
+            throw new TypeError(
+              `Generated invite code is not a string of the correct length! ` +
+                `Expected length of '${INVITE_CODE_LENGTH}'', received length of '${invite_code.length}'`,
+            );
+          } else {
+            cy.log(
+              `Successfully generated random invite_code: '${invite_code}'`,
+            );
+          }
 
-        const max_uses: number = 1;
+          const max_uses: number = 1;
 
-        cy.create_invite_code(invite_code, max_uses).then(() => {
-          cy.log(
-            `Successfully created invite code '${invite_code}' with max uses ${max_uses}`,
-          );
-        });
-      })
+          cy.create_invite_code(invite_code, max_uses).then(() => {
+            cy.log(
+              `Successfully created invite code '${invite_code}' with max uses ${max_uses}`,
+            );
+          });
+        },
+      );
     });
   });
 });
--- tests/e2e-auth-tests/cypress/support/actions/create_invite_code.ts
+++ tests/e2e-auth-tests/cypress/support/actions/create_invite_code.ts
@@ -1,68 +1,97 @@
+const createInviteCodeDialogContentId: string =
+  "create-invite-code-dialog-content";
+const openInviteCodeCreationDialogButtonId: string =
+  "open-create-invite-code-dialog-button";
+const submitInviteCodeCreationDialogButtonId: string =
+  "submit-create-invite-code-form-button";
 
-const createInviteCodeDialogContentId: string = "create-invite-code-dialog-content";
-const openInviteCodeCreationDialogButtonId: string = "open-create-invite-code-dialog-button";
-const submitInviteCodeCreationDialogButtonId: string = "submit-create-invite-code-form-button";
-
-export default function createInviteCode(invite_code: string, max_uses: number): Cypress.Chainable<boolean> {
-  if (typeof invite_code !== 'string') {
+export default function createInviteCode(
+  invite_code: string,
+  max_uses: number,
+): Cypress.Chainable<boolean> {
+  if (typeof invite_code !== "string") {
     throw new TypeError("'invite_code' must be a string");
   }
 
-  if (typeof max_uses !== 'number') {
+  if (typeof max_uses !== "number") {
     throw new TypeError("'max_uses' must be a number");
   }
-  
+
   return cy.is_admin().then((isAdmin) => {
     if (!isAdmin) {
-      throw new Error("Cannot create invite code: current user is not an admin.");
+      throw new Error(
+        "Cannot create invite code: current user is not an admin.",
+      );
     }
 
     return cy.visit("/admin/invite_codes").then(() => {
       cy.url().should("include", "/admin/invite_codes");
 
-      cy.get(`#${createInviteCodeDialogContentId}`, { log: false }).should("not.exist");
+      cy.get(`#${createInviteCodeDialogContentId}`, { log: false }).should(
+        "not.exist",
+      );
 
-      cy.get(`button#${openInviteCodeCreationDialogButtonId}`).should('exist').click();
+      cy.wait(500);
 
-      cy.wait(500); // Wait for dialog animation
-
-      cy.get(`#${createInviteCodeDialogContentId}`, { log: false }).should("be.visible");
-      cy.url({ log: false }).should("include", "/admin/invite_codes");
-
-      // Fill out form within new dialog
-      cy.get(`input[name="invite_code"]`, { log: false })
+      cy.get(`button#${openInviteCodeCreationDialogButtonId}`)
         .should("exist")
-        .should('be.visible')
-        .should("not.be.disabled")
-        .type(invite_code, { force: true });
-
-      cy.get(`textarea[name="description"]`, { log: false })
-        .should("exist")
-        .should('be.visible')
-        .should("not.be.disabled")
-        .type("Invite code generated within Cypress E2E test", { force: true });
-
-      cy.url({ log: false }).should("include", "/admin/invite_codes");
-
-      cy.get(`input[name="max_uses"]`, { log: false })
-        .should("exist")
-        .should('be.visible')
-        .should("not.be.disabled")
-        .type(`{selectAll}${max_uses.toString()}`, { force: true });
-
-      // Submit form
-      cy.intercept("POST", "**/api/admin/invite-codes/create").as("createInviteCodeRequest");
-      cy.get(`button#${submitInviteCodeCreationDialogButtonId}`, { log: false })
-        .should("exist")
-        .should("not.be.disabled")
         .click();
 
-      return cy.wait("@createInviteCodeRequest", { timeout: 10000 }).then((interception) => {
-        interception.response?.statusCode && cy.wrap(interception.response?.statusCode).should("eq", 200);
-        cy.log("Invite code creation request appears to have been a success!");
-        cy.get(`#${createInviteCodeDialogContentId}`, { log: false }).should("not.exist");
-        return true;
+      // Wait for dialog animation
+      return cy.wait(1000).then(() => {
+        cy.get(`#${createInviteCodeDialogContentId}`, { log: false }).should(
+          "be.visible",
+        );
+        cy.url({ log: false }).should("include", "/admin/invite_codes");
+
+        // Fill out form within new dialog
+        cy.get(`input[name="invite_code"]`, { log: false })
+          .should("exist")
+          .should("be.visible")
+          .should("not.be.disabled")
+          .type(invite_code, { force: true });
+
+        cy.get(`textarea[name="description"]`, { log: false })
+          .should("exist")
+          .should("be.visible")
+          .should("not.be.disabled")
+          .type("Invite code generated within Cypress E2E test", {
+            force: true,
+          });
+
+        cy.url({ log: false }).should("include", "/admin/invite_codes");
+
+        cy.get(`input[name="max_uses"]`, { log: false })
+          .should("exist")
+          .should("be.visible")
+          .should("not.be.disabled")
+          .type(`{selectAll}${max_uses.toString()}`, { force: true });
+
+        // Submit form
+        cy.intercept("POST", "**/api/admin/invite-codes/create").as(
+          "createInviteCodeRequest",
+        );
+        cy.get(`button#${submitInviteCodeCreationDialogButtonId}`, {
+          log: false,
+        })
+          .should("exist")
+          .should("not.be.disabled")
+          .click();
+
+        return cy
+          .wait("@createInviteCodeRequest", { timeout: 10000 })
+          .then((interception) => {
+            interception.response?.statusCode &&
+              cy.wrap(interception.response?.statusCode).should("eq", 200);
+            cy.log(
+              "Invite code creation request appears to have been a success!",
+            );
+            cy.get(`#${createInviteCodeDialogContentId}`, {
+              log: false,
+            }).should("not.exist");
+            return true;
+          });
       });
-    })
-  })
+    });
+  });
 }
