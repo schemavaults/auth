6219f99c8a231837c28b232774e3a5107e48a876
jalexw <jalexwhitman@gmail.com>
2026-01-16 16:00:31 -0400

    auth-ui:0.5.43 - fix parsing app_id in AppsTable

--- packages/auth-ui/src/components/AppsTable/columns.tsx
+++ packages/auth-ui/src/components/AppsTable/columns.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import type { ReactElement } from "react"
+import type { ReactElement } from "react";
 
-import type { ColumnDef } from "@tanstack/react-table"
+import type { ColumnDef } from "@tanstack/react-table";
 import { Checkbox } from "@schemavaults/ui";
-import type { ListAppsQueryType, SchemaVaultsApp } from "@schemavaults/app-definitions";
+import type {
+  ListAppsQueryType,
+  SchemaVaultsApp,
+} from "@schemavaults/app-definitions";
 import { FrontendApplicationActions } from "./frontend_app_actions";
 import { AppDomainsList } from "./AppDomainsList";
 import type { PreloadedAppsTableDataWithDomainRefs } from "./preloaded_apps_table_data";
 
 export function getAppsTableColumns(
   queryType: ListAppsQueryType,
-  preloaded?: PreloadedAppsTableDataWithDomainRefs
+  preloaded?: PreloadedAppsTableDataWithDomainRefs,
 ): ColumnDef<SchemaVaultsApp>[] {
   const columns: ColumnDef<SchemaVaultsApp>[] = [
     {
@@ -22,7 +25,9 @@ export function getAppsTableColumns(
             table.getIsAllPageRowsSelected() ||
             (table.getIsSomePageRowsSelected() && "indeterminate")
           }
-          onCheckedChange={(value: boolean) => table.toggleAllPageRowsSelected(!!value)}
+          onCheckedChange={(value: boolean) =>
+            table.toggleAllPageRowsSelected(!!value)
+          }
           aria-label="Select all"
         />
       ),
@@ -37,19 +42,19 @@ export function getAppsTableColumns(
       enableHiding: false,
     },
     {
-      id: 'app_id',
+      id: "app_id",
       accessorKey: "app_id",
       header: "App ID",
     },
     {
-      id: 'app_name',
+      id: "app_name",
       accessorKey: "app_name",
-      header: "App Name"
+      header: "App Name",
     },
     {
-      id: 'app_description',
+      id: "app_description",
       accessorKey: "app_description",
-      header: "Description"
+      header: "Description",
     },
     {
       id: "domains",
@@ -57,21 +62,22 @@ export function getAppsTableColumns(
       cell: ({ row }) => {
         let app_id: string | undefined = undefined;
         try {
-          const id: unknown = row.getValue('id');
-          if (typeof id !== 'string') {
+          const id: unknown = row.getValue("app_id");
+          if (typeof id !== "string") {
             throw new Error("Expected ID to be a string");
           }
           app_id = id;
         } catch (e: unknown) {
-          console.error("Failed to load app ID for 'domains' cell in AppsTable: ", e);
+          console.error(
+            "Failed to load app ID for 'domains' cell in AppsTable: ",
+            e,
+          );
           app_id = undefined;
         }
 
-        if (typeof app_id !== 'string') {
+        if (typeof app_id !== "string") {
           return (
-            <p className="text-destructive">
-              Invalid app ID; not a string
-            </p>
+            <p className="text-destructive">Invalid app ID; not a string</p>
           );
         }
 
@@ -79,8 +85,9 @@ export function getAppsTableColumns(
           <AppDomainsList
             app_id={app_id}
             preloaded_domains={
-              (typeof preloaded === 'object' && !!preloaded) ?
-                preloaded.domains[app_id] : undefined
+              typeof preloaded === "object" && !!preloaded
+                ? preloaded.domains[app_id]
+                : undefined
             }
           />
         );
@@ -91,16 +98,11 @@ export function getAppsTableColumns(
     {
       id: "actions",
       cell: ({ row }): ReactElement => {
-        const app: SchemaVaultsApp = row.original
+        const app: SchemaVaultsApp = row.original;
 
-        return (
-          <FrontendApplicationActions
-            app={app}
-            queryType={queryType}
-          />
-        );
+        return <FrontendApplicationActions app={app} queryType={queryType} />;
       },
-    }
+    },
   ];
   return columns;
 }
