8b5d84e14f403b259c84b3350e4f453fb12c14dd
jalexw <jalexwhitman@gmail.com>
2026-01-20 12:25:30 -0400

    app-definitions:0.6.4 - updating list api servers query type

--- packages/app-definitions/src/list-registry-api-servers-query-params.ts
+++ packages/app-definitions/src/list-registry-api-servers-query-params.ts
@@ -1,24 +1,29 @@
-
 import { z } from "zod";
 import type { SchemaVaultsApiServerDefinition } from "./api-server-definition";
 
 const list_api_servers_query_type = [
-  'all', // admin-only
+  "all", // admin-only
+  "org",
 ] as const satisfies string[];
 
-export type ListApiServersQueryType = typeof list_api_servers_query_type[number];
+export type ListApiServersQueryType =
+  (typeof list_api_servers_query_type)[number];
 
-export const listApiServersQueryTypeSchema = z.string()
+export const listApiServersQueryTypeSchema = z
+  .string()
   .refine(
-    (val: string): val is ListApiServersQueryType => (list_api_servers_query_type as string[]).includes(val),
-    `Invalid list API servers query type. Should be one of: ${list_api_servers_query_type.map(val=>`"${val}"`).join(", ")}`
-  )
+    (val: string): val is ListApiServersQueryType =>
+      (list_api_servers_query_type as string[]).includes(val),
+    `Invalid list API servers query type. Should be one of: ${list_api_servers_query_type.map((val) => `"${val}"`).join(", ")}`,
+  );
 
-export type ListApiServersQueryResponse = {
-  success: true;
-  message: string;
-  list: SchemaVaultsApiServerDefinition[];
-} | {
-  success: false;
-  message: string;
-}
+export type ListApiServersQueryResponse =
+  | {
+      success: true;
+      message: string;
+      list: readonly SchemaVaultsApiServerDefinition[];
+    }
+  | {
+      success: false;
+      message: string;
+    };
