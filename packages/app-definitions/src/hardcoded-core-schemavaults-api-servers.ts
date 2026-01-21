bc915305f4eef3c063f57fb092c3b7e100c0724b
jalexw <jalexwhitman@gmail.com>
2026-01-20 20:37:33 -0400

    app-defintitions:0.6.7 - add schemavaults-auth as hardcoded api server

--- packages/app-definitions/src/hardcoded-core-schemavaults-api-servers.ts
+++ packages/app-definitions/src/hardcoded-core-schemavaults-api-servers.ts
@@ -1,8 +1,19 @@
 import type { SchemaVaultsApiServerDefinition } from "./api-server-definition";
+import { SCHEMAVAULTS_AUTH_APP_DEFINITION } from "./hardcoded-core-schemavaults-apps";
 
 const createdAt: number = new Date(
   new Date(new Date().setFullYear(2024)).setMonth(7),
 ).setDate(27);
+
+export const SCHEMAVAULTS_AUTH_SERVER = {
+  api_server_id: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
+  api_server_name: "SchemaVaults Auth",
+  api_server_description: SCHEMAVAULTS_AUTH_APP_DEFINITION.app_description,
+  public: true,
+  created_at: createdAt,
+  hardcoded: true,
+  owner_organization_id: "schemavaults",
+} as const satisfies SchemaVaultsApiServerDefinition;
 
 export const SCHEMAVAULTS_REGISTRY_SERVER = {
   api_server_id: "schemavaults-registry",
@@ -28,6 +39,7 @@ export const SCHEMAVAULTS_MAIL_SERVER = {
 export const HARDCODED_CORE_SCHEMAVAULTS_API_SERVERS = [
   SCHEMAVAULTS_REGISTRY_SERVER,
   SCHEMAVAULTS_MAIL_SERVER,
+  SCHEMAVAULTS_AUTH_SERVER,
 ] as const satisfies readonly SchemaVaultsApiServerDefinition[];
 
 export default HARDCODED_CORE_SCHEMAVAULTS_API_SERVERS;
