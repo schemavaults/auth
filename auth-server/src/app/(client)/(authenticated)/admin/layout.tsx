6afa9ed209c3e3678cbd9c0ad6eeabbbfa3888c8
jalexw <jalexwhitman@gmail.com>
2026-01-23 10:38:39 -0400

    auth-server:0.14.5 - use withAdminRouteGuard for /admin server layout.tsx

--- auth-server/src/app/(client)/(authenticated)/admin/layout.tsx
+++ auth-server/src/app/(client)/(authenticated)/admin/layout.tsx
@@ -1,75 +1,21 @@
 import "server-only";
 
 import redirectWithError from "@/lib/redirect-with-error";
-import {
-  getAppEnvironment,
-  SCHEMAVAULTS_AUTH_APP_DEFINITION,
-  type SchemaVaultsAppEnvironment,
-} from "@schemavaults/app-definitions";
-import type { PotentiallyValidTokenSource } from "@schemavaults/auth-common";
-import RouteGuardFactory from "@/lib/RouteGuardFactory";
-import { cookies } from "next/headers";
-import type { ReactNode } from "react";
-import { ServerlessDatabase } from "@/lib/auth-db";
-import shouldEnableDebug from "@/lib/should-enable-debug";
+import type { ReactElement, ReactNode } from "react";
+import { type IProtectedAdminServerComponentPageProps, withAdminServerComponentRouteGuard } from "@/lib/withAdminRouteGuard";
 
 export default async function AdminPathsRouteGuardServerComponent({
   children,
 }: {
   children: ReactNode;
-}): Promise<ReactNode> {
-  const environment: SchemaVaultsAppEnvironment = getAppEnvironment();
-  const debug: boolean = shouldEnableDebug(environment)
-  if (debug) {
-    console.log("[AdminPathsRouteGuardServerComponent] Preparing admin page...");
-  }
-
-  await using dbh = ServerlessDatabase.createDBH();
-
-  const token_sources: PotentiallyValidTokenSource[] = [];
-
-  const refresh_token_cookie = (await cookies()).get("refresh_token");
-  if (typeof refresh_token_cookie?.value === "string") {
-    token_sources.push({
-      sourceHint: "Auth Server Refresh Token",
-      type: "refresh",
-      token: refresh_token_cookie.value,
-    });
-  } else {
-    if (debug) {
-      console.warn("[AdminPathsRouteGuardServerComponent] No refresh token cookie found.");
+  }): Promise<ReactNode> {
+  const protected_layout: ReactElement = await withAdminServerComponentRouteGuard(
+    async function AdminProtectedChildPage({ user }: IProtectedAdminServerComponentPageProps): Promise<ReactElement> {
+      if (!user || !user.admin) {
+        redirectWithError(403, "forbidden")
+      }
+      return <>{children}</>
     }
-  }
-
-  const route_guard_factory = new RouteGuardFactory(dbh.db);
-  const route_guard = await route_guard_factory.createGuardFromTokenSources(
-    "admin",
-    token_sources,
-    SCHEMAVAULTS_AUTH_APP_DEFINITION.app_id,
-  );
-  if (!route_guard.isAccessAllowed) {
-    redirectWithError(
-      401,
-      "unauthenticated",
-    );
-  }
-  const user = route_guard.user;
-  if (!user) {
-    redirectWithError(
-      401,
-      "load_user_data_failure",
-    );
-  }
-  if (!user) {
-    // allow typescript to see that user data is set
-    throw new Error(
-      "This should be unreachable code-- redirectWithError seems to not have worked!",
-    );
-  } else if (!user.admin) {
-    throw new Error(
-      "This should be unreachable code-- redirectWithError seems to not have redirected user despite not being an admin!",
-    );
-  }
-
-  return children;
+  )
+  return protected_layout;
 }
