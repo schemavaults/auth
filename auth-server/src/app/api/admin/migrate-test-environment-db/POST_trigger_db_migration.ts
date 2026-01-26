bc407ba3d41c522456e31e3b6ff3a8041388547f
jalexw <jalexwhitman@gmail.com>
2026-01-26 11:18:11 -0400

    reorganize endpoint for better error logging

--- auth-server/src/app/api/admin/migrate-test-environment-db/POST_trigger_db_migration.ts
+++ auth-server/src/app/api/admin/migrate-test-environment-db/POST_trigger_db_migration.ts
@@ -8,56 +8,62 @@ import { type NextRequest, NextResponse } from "next/server";
 import type { AuthDatabase } from "@/lib/auth-db/auth-database-types";
 import type { Kysely } from "@schemavaults/dbh";
 import { getAppEnvironment, type SchemaVaultsAppEnvironment } from "@schemavaults/app-definitions";
+import shouldEnableDebug from "@/lib/should-enable-debug";
 
 async function trigger_db_migration(
   db: Kysely<AuthDatabase>
-): Promise<NextResponse> {
-  try {
-    const migrateToLatest: (db: Kysely<any>) => Promise<void> = await import(
-      "@/lib/auth-db/migrate-to-latest"
-    ).then(mod => mod.default);
-    await migrateToLatest(db);
-  } catch (e: unknown) {
-    const baseErrorMessage: string = "Error attempting to initialize @schemavaults/auth-server postgres database";
-    console.error(
-      `${baseErrorMessage}: `,
-      e,
-    );
-
-    let errorMessage: string = baseErrorMessage;
-    if (e instanceof Error) {
-      errorMessage += ". ";
-      errorMessage += e.message;
-    }
-
-    return NextResponse.json(
-      {
-        success: false,
-        message: errorMessage,
-      } satisfies ResourceCreationResponse,
-      {
-        status: 500,
-      },
-    );
-  }
-
-  return NextResponse.json(
-    {
-      success: true,
-      message: "Initialized @schemavaults/auth-server postgres database!",
-    },
-    {
-      status: 200,
-    },
-  );
+): Promise<void> {
+  const migrateToLatest: (db: Kysely<any>) => Promise<void> = await import(
+    "@/lib/auth-db/migrate-to-latest"
+  ).then(mod => mod.default);
+  await migrateToLatest(db);
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const environment: SchemaVaultsAppEnvironment = getAppEnvironment();
+  const debug: boolean = shouldEnableDebug(environment);
 
   if (environment === 'test') {
     await using dbh = ServerlessDatabase.createDBH();
-    return await trigger_db_migration(dbh.db)
+
+    try {
+      if (debug) {
+        console.log("")
+      }
+      await trigger_db_migration(dbh.db)
+    } catch (e: unknown) {
+      const baseErrorMessage: string = "Error attempting to initialize @schemavaults/auth-server postgres database";
+      console.error(
+        `${baseErrorMessage}: `,
+        e,
+      );
+
+      let errorMessage: string = baseErrorMessage;
+      if (e instanceof Error) {
+        errorMessage += ". ";
+        errorMessage += e.message;
+      }
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: errorMessage,
+        } satisfies ResourceCreationResponse,
+        {
+          status: 500,
+        },
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Initialized @schemavaults/auth-server postgres database!",
+      },
+      {
+        status: 200,
+      },
+    );
   } else {
     return NextResponse.json({
       error: "Route not available in this environment",
