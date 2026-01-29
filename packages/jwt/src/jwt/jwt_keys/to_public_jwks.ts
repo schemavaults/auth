e7b9c3d28f875897ad66bb14e365e1b7734fe2ab
jalexw <jalexwhitman@gmail.com>
2026-01-28 20:04:42 -0400

    jwt:0.6.21 - dont throw for empty jwks

--- packages/jwt/src/jwt/jwt_keys/to_public_jwks.ts
+++ packages/jwt/src/jwt/jwt_keys/to_public_jwks.ts
@@ -49,7 +49,7 @@ export async function to_public_jwks(
 
   const keys: readonly JWK[] = output_jwks;
   if (keys.length === 0) {
-    throw new Error("[to_public_jwks] Output 'keys' array is empty!");
+    console.warn("[to_public_jwks] Output 'keys' array is empty!");
   }
 
   return {
