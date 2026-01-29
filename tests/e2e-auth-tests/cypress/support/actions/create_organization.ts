7a29bd07509984a1f9b4bb6a1a79375beea4cc9a
jalexw <jalexwhitman@gmail.com>
2026-01-29 14:20:45 -0400

    e2e-auth-tests:0.1.12 - validate that org creation form fields are filled correctly before submitting

--- tests/e2e-auth-tests/cypress/support/actions/create_organization.ts
+++ tests/e2e-auth-tests/cypress/support/actions/create_organization.ts
@@ -32,81 +32,94 @@ export default function createOrganization(
 
     return cy.visit("/admin/organizations").then(() => {
       cy.url().should("include", "/admin/organizations");
-      cy.wait_for_page_hydration();
+      return cy.wait_for_page_hydration().then(() => {
+        return cy
+          .open_dialog_with_button(
+            openOrganizationCreationDialogButtonId,
+            createOrganizationDialogContentId,
+          )
+          .then(() => {
+            cy.url({ log: false }).should("include", "/admin/organizations");
 
-      return cy
-        .open_dialog_with_button(
-          openOrganizationCreationDialogButtonId,
-          createOrganizationDialogContentId,
-        )
-        .then(() => {
-          cy.url({ log: false }).should("include", "/admin/organizations");
+            // Fill out form within new dialog
+            cy.get(`input[name="organization_id"]`, { log: false })
+              .should("exist")
+              .should("not.be.disabled")
+              .type(organization_id);
 
-          // Fill out form within new dialog
-          cy.get(`input[name="organization_id"]`, { log: false })
-            .should("exist")
-            .should("not.be.disabled")
-            .type(organization_id);
+            cy.log(`Finished typing organization ID "${organization_id}"`);
 
-          cy.log(`Finished typing organization ID "${organization_id}"`);
+            cy.get(`input[name="name"]`, { log: false })
+              .should("exist")
+              .should("not.be.disabled")
+              .type(name);
 
-          cy.get(`input[name="name"]`, { log: false })
-            .should("exist")
-            .should("not.be.disabled")
-            .type(name);
+            cy.log(`Finished typing organization name "${name}"`);
 
-          cy.log(`Finished typing organization name "${name}"`);
+            // Form should now be filled-- validate state before submitting
+            cy.get(`#${createOrganizationDialogContentId}`, {
+              log: false,
+            }).should("exist");
+            cy.url({ log: false }).should("include", "/admin/organizations");
 
-          cy.get(`#${createOrganizationDialogContentId}`, {
-            log: false,
-          }).should("exist");
-          cy.url({ log: false }).should("include", "/admin/organizations");
+            // Validate organization ID was filled correctly
+            cy.get(`input[name="organization_id"]`, { log: false }).should(
+              "have.value",
+              organization_id,
+            );
 
-          // Set up interception for org creation request
-          cy.intercept({
-            method: "POST",
-            url: "**/api/organizations",
-            times: 1,
-          }).as("createOrganizationRequest");
+            // Validate organization name was filled correctly
+            cy.get(`input[name="name"]`, { log: false }).should(
+              "have.value",
+              name,
+            );
 
-          cy.log("Interceptor prepared. Clicking on submit button.");
+            // Set up interception for org creation request
+            cy.intercept({
+              method: "POST",
+              url: "**/api/organizations",
+              times: 1,
+            }).as("createOrganizationRequest");
 
-          // Click on submit button
-          cy.get(`button#${submitOrganizationCreationDialogButtonId}`, {
-            log: false,
-          })
-            .should("exist")
-            .should("not.be.disabled")
-            .click();
+            cy.log("Interceptor prepared. Clicking on submit button.");
 
-          cy.log("Create organization form submitted!");
-          cy.has_error_toast().then((error: boolean) => {
-            if (error) {
-              cy.log_active_toasts();
-            }
-          });
-
-          return cy
-            .wait("@createOrganizationRequest", {
-              timeout: 20000,
-              requestTimeout: 20000,
+            // Click on submit button
+            cy.get(`button#${submitOrganizationCreationDialogButtonId}`, {
+              log: false,
             })
-            .then((interception) => {
-              interception.response?.statusCode &&
-                cy.wrap(interception.response?.statusCode).should("eq", 200);
-              cy.log(
-                "Organization creation request appears to have been a success!",
-              );
-              cy.get(`#${createOrganizationDialogContentId}`, {
-                log: false,
-              }).should("not.exist");
-              return cy.wrap(true, { log: false });
-            })
-            .then((val): boolean => {
-              if (typeof val === "boolean") return val;
-              return val[0];
+              .should("exist")
+              .should("not.be.disabled")
+              .click();
+
+            cy.log("Create organization form submitted!");
+            cy.has_error_toast().then((error: boolean) => {
+              if (error) {
+                cy.log_active_toasts();
+              }
             });
-        });
+
+            return cy
+              .wait("@createOrganizationRequest", {
+                timeout: 20000,
+                requestTimeout: 20000,
+              })
+              .then((interception) => {
+                interception.response?.statusCode &&
+                  cy.wrap(interception.response?.statusCode).should("eq", 200);
+                cy.log(
+                  "Organization creation request appears to have been a success!",
+                );
+                cy.get(`#${createOrganizationDialogContentId}`, {
+                  log: false,
+                }).should("not.exist");
+                return cy.wrap(true, { log: false });
+              })
+              .then((val): boolean => {
+                if (typeof val === "boolean") return val;
+                return val[0];
+              });
+          });
+      });
     });
   });
 }
