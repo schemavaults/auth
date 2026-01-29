970104301b5f9f15ddd0538d2a922b0bd4ff2636
jalexw <jalexwhitman@gmail.com>
2026-01-28 23:01:51 -0400

    debug logging around invite code validation

--- auth-server/src/lib/auth-db/users/validate-invite-code-definition.ts
+++ auth-server/src/lib/auth-db/users/validate-invite-code-definition.ts
@@ -3,15 +3,20 @@ import {
   type InviteCodeDefinition,
   inviteCodeDefinitionSchema,
 } from "@schemavaults/auth-common";
+import { getAppEnvironment } from "@schemavaults/app-definitions";
 
 export function isValidInviteCodeDefinition(
   maybe_invite_code_definition: object,
 ): maybe_invite_code_definition is InviteCodeDefinition {
+  const environment = getAppEnvironment();
   const parsed = inviteCodeDefinitionSchema.safeParse(
     maybe_invite_code_definition,
   );
   if (!parsed.success) {
-    console.error("Invalid invite code definition: ", parsed.error);
+    console.error("[isValidInviteCodeDefinition] Invalid invite code definition: ", parsed.error);
+    if (environment === 'development' || environment === 'test') {
+      console.error(maybe_invite_code_definition)
+    }
     return false;
   }
   return true;
@@ -21,6 +26,7 @@ export function areValidInviteCodeDefinitions(
   maybe_invite_code_definitions: readonly object[],
 ): maybe_invite_code_definitions is readonly InviteCodeDefinition[] {
   if (!Array.isArray(maybe_invite_code_definitions)) {
+    console.warn("areValidInviteCodeDefinitions did not receive an array")
     return false;
   }
   if (maybe_invite_code_definitions.length === 0) {
