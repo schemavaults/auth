80dbf1d7f48c6625e16ff1185f97e162a90194f6
jalexw <jalexwhitman@gmail.com>
2026-02-03 18:03:24 -0400

    cy.log() within is_admin and is_authenticated

--- tests/e2e-auth-tests/cypress/support/actions/is_admin.ts
+++ tests/e2e-auth-tests/cypress/support/actions/is_admin.ts
@@ -1,19 +1,26 @@
-
 export default function is_admin(): Cypress.Chainable<boolean> {
-  return cy.request({
-    method: "GET",
-    url: "/api/auth/whoami",
-    failOnStatusCode: false,
-  }).then((response) => {
-    if (response.status === 200) {
-      const body = response.body;
-      if (typeof body === 'object' && body && 'user' in body) {
-        const user = body.user;
-        if (user && typeof user === 'object' && 'admin' in user && user.admin === true) {
-          return true;
+  cy.log(`[cy.is_admin()] Checking if authenticated as an admin...`);
+  return cy
+    .request({
+      method: "GET",
+      url: "/api/auth/whoami",
+      failOnStatusCode: false,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        const body = response.body;
+        if (typeof body === "object" && body && "user" in body) {
+          const user = body.user;
+          if (
+            user &&
+            typeof user === "object" &&
+            "admin" in user &&
+            user.admin === true
+          ) {
+            return true;
+          }
         }
       }
-    }
-    return false;
-  });
+      return false;
+    });
 }
--- tests/e2e-auth-tests/cypress/support/actions/is_authenticated.ts
+++ tests/e2e-auth-tests/cypress/support/actions/is_authenticated.ts
@@ -1,13 +1,16 @@
 export default function is_authenticated(): Cypress.Chainable<boolean> {
-  return cy.request({
-    method: "GET",
-    url: "/api/auth/whoami",
-    failOnStatusCode: false,
-  }).then((response) => {
-    if (response.status === 200) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  cy.log(`[cy.is_authenticated()] Checking if authenticated...`);
+  return cy
+    .request({
+      method: "GET",
+      url: "/api/auth/whoami",
+      failOnStatusCode: false,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        return true;
+      } else {
+        return false;
+      }
+    });
 }
