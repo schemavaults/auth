0c425d4223b75877be17d3343a4a8ab5e5c858bd
jalexw <jalexwhitman@gmail.com>
2026-02-06 22:16:41 -0400

    auth-server:0.17.4 - type hint on whoami route, package updates for auth-client-sdk and auth-react-provider

--- auth-server/src/app/api/auth/whoami/route.ts
+++ auth-server/src/app/api/auth/whoami/route.ts
@@ -4,16 +4,18 @@ import { type NextRequest, NextResponse } from "next/server";
 import { type IProtectedAuthenticatedApiRouteProps, withAuthenticatedApiRouteGuard } from "@/lib/withAuthenticatedRouteGuard";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
-  const protected_route = await withAuthenticatedApiRouteGuard(async ({ user }: IProtectedAuthenticatedApiRouteProps) => {
-    return NextResponse.json(
-      {
-        success: true,
-        user: user,
-      },
-      {
-        status: 200,
-      },
-    );
-  });
+  const protected_route = await withAuthenticatedApiRouteGuard(
+    async ({ user }: IProtectedAuthenticatedApiRouteProps): Promise<NextResponse> => {
+      return NextResponse.json(
+        {
+          success: true,
+          user: user,
+        },
+        {
+          status: 200,
+        },
+      );
+    }
+  );
   return await protected_route(req);
 }
