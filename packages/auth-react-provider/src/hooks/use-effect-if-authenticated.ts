a130963498fa66ef87c05b480ecde4ad9a1c47bc
jalexw <jalexwhitman@gmail.com>
2026-02-09 13:40:34 -0400

    auth-react-provider:0.7.5 - debugging useEffectIfAuthenticated

--- packages/auth-react-provider/src/hooks/use-effect-if-authenticated.ts
+++ packages/auth-react-provider/src/hooks/use-effect-if-authenticated.ts
@@ -3,6 +3,10 @@
 import { useEffect } from "react";
 import useAuth from "./use-auth";
 import type { ISchemaVaultsAuthClient } from "@schemavaults/auth-client-sdk";
+import useDebug from "./use-debug";
+import useAppEnvironment, {
+  type SchemaVaultsAppEnvironment,
+} from "./use-app-environment";
 
 type UnsubscribeFn = () => void;
 
@@ -10,17 +14,34 @@ export function useEffectIfAuthenticated(
   effect: (auth: ISchemaVaultsAuthClient) => UnsubscribeFn,
 ) {
   const authContext = useAuth();
+  const environment: SchemaVaultsAppEnvironment = useAppEnvironment();
+  const debug: boolean = useDebug(environment);
 
   useEffect(() => {
     if (!authContext.ready || !authContext.client.current) {
       return;
     }
     const auth: ISchemaVaultsAuthClient = authContext.client.current;
-    if (auth.isAuthenticated) {
+    const isAuthenticated: boolean = auth.isAuthenticated;
+    if (debug) {
+      console.log(
+        `[useEffectIfAuthenticated] isAuthenticated = ${isAuthenticated ? "True" : "False"}`,
+      );
+    }
+    if (isAuthenticated) {
+      if (debug) {
+        console.log(`[useEffectIfAuthenticated] Running 'effect' fn...`);
+      }
+      if (typeof effect !== "function") {
+        throw new TypeError("Expected 'effect' to be a function!");
+      }
       const unsubscribe: UnsubscribeFn = effect(auth);
+      if (typeof unsubscribe !== "function") {
+        throw new TypeError("Expected 'unsubscribe' to be a function!");
+      }
       return unsubscribe;
     }
-  }, [authContext, effect]);
+  }, [authContext, effect, debug]);
 }
 
 export default useEffectIfAuthenticated;
