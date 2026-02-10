42fed143f8936887378dfa69f6fd8f34823ccd8e
jalexw <jalexwhitman@gmail.com>
2026-02-10 10:42:28 -0400

    auth-server-sdk:0.19.7 - fix resolve codegen-templates/ directory

--- packages/auth-server-sdk/src/NextjsAppDirectoryPlugin/resolve-codegen-templates-directory.ts
+++ packages/auth-server-sdk/src/NextjsAppDirectoryPlugin/resolve-codegen-templates-directory.ts
@@ -1,4 +1,5 @@
-import { dirname, join } from "path";
+import { dirname, join, normalize } from "path";
+import { existsSync } from "fs";
 
 export default function resolveCodegenTemplatesDirectory(): string {
   if (!__dirname) {
@@ -8,5 +9,18 @@ export default function resolveCodegenTemplatesDirectory(): string {
   }
 
   // Templates are copied into the dist/codegen-templates directory during build
-  return join(dirname(__dirname), "codegen-templates");
+  // ... but sometimes this runs from src/NextjsAppDirectoryPlugin/resolve-codegen-templates-directory.ts
+  // ... while other times it is running from the bundled version at dist/cli.cjs
+
+  if (existsSync(join(__dirname, "codegen-templates"))) {
+    return normalize(join(__dirname, "codegen-templates"));
+  } else if (existsSync(join(dirname(__dirname), "codegen-templates"))) {
+    return normalize(join(dirname(__dirname), "codegen-templates"));
+  } else if (
+    existsSync(join(dirname(__dirname), "dist", "codegen-templates"))
+  ) {
+    return normalize(join(dirname(__dirname), "dist", "codegen-templates"));
+  }
+
+  throw new Error("Failed to resolve 'codegen-templates' directory!");
 }
