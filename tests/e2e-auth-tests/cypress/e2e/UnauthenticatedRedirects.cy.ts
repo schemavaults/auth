f5557394ed39978d7a34ef943022686da449cc6c
jalexw <jalexwhitman@gmail.com>
2026-03-04 18:01:16 -0400

    e2e-auth-tests:0.1.24 - add pages to UnauthenticatedRedirects tests

--- tests/e2e-auth-tests/cypress/e2e/UnauthenticatedRedirects.cy.ts
+++ tests/e2e-auth-tests/cypress/e2e/UnauthenticatedRedirects.cy.ts
@@ -1,11 +1,59 @@
 describe("Unauthenticated Redirects", () => {
-  it("is redirected off the account page", () => {
-    cy.visit("/account");
-    cy.url().should("not.include", "/account");
+  describe("Authenticated pages", () => {
+    it("is redirected off the account page", () => {
+      cy.visit("/account");
+      cy.url().should("not.include", "/account");
+    });
+
+    it("is redirected off the org page", () => {
+      cy.visit("/org/fake-org-id");
+      cy.url().should("not.include", "/org/fake-org-id");
+    });
+
+    it("is redirected off the JWKS access keys page", () => {
+      const fakeApiServerId = crypto.randomUUID();
+      cy.visit(`/apis/${fakeApiServerId}/jwks-access-keys`);
+      cy.url().should(
+        "not.include",
+        `/apis/${fakeApiServerId}/jwks-access-keys`,
+      );
+    });
   });
 
-  it("is redirected off the admin page", () => {
-    cy.visit("/admin");
-    cy.url().should("not.include", "/admin");
+  describe("Admin pages", () => {
+    it("is redirected off the admin page", () => {
+      cy.visit("/admin");
+      cy.url().should("not.include", "/admin");
+    });
+
+    it("is redirected off the admin users page", () => {
+      cy.visit("/admin/users");
+      cy.url().should("not.include", "/admin/users");
+    });
+
+    it("is redirected off the admin apps page", () => {
+      cy.visit("/admin/apps");
+      cy.url().should("not.include", "/admin/apps");
+    });
+
+    it("is redirected off the admin apis page", () => {
+      cy.visit("/admin/apis");
+      cy.url().should("not.include", "/admin/apis");
+    });
+
+    it("is redirected off the admin organizations page", () => {
+      cy.visit("/admin/organizations");
+      cy.url().should("not.include", "/admin/organizations");
+    });
+
+    it("is redirected off the admin invite codes page", () => {
+      cy.visit("/admin/invite_codes");
+      cy.url().should("not.include", "/admin/invite_codes");
+    });
+
+    it("is redirected off the admin settings page", () => {
+      cy.visit("/admin/settings");
+      cy.url().should("not.include", "/admin/settings");
+    });
   });
 });
