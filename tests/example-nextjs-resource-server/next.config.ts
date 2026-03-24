acbdd104d8607152ed8c35066ad8879b5b14f7ff
jalexw <jalexwhitman@gmail.com>
2026-03-24 13:43:15 -0300

    example-nextjs-resource-server:0.1.3 - build as standalone within docker image

--- tests/example-nextjs-resource-server/next.config.ts
+++ tests/example-nextjs-resource-server/next.config.ts
@@ -1,8 +1,16 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
+function isNextStandaloneDockerBuildFlagSet(): boolean {
+  return (
+    typeof process.env.NEXT_STANDALONE_DOCKER_BUILD === "string" &&
+    process.env.NEXT_STANDALONE_DOCKER_BUILD.length > 0
+  );
+}
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: isNextStandaloneDockerBuildFlagSet() ? "standalone" : undefined,
 };
 
 export default nextConfig;
