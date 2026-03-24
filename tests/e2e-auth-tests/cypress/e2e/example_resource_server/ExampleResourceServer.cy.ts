f3a28cf20175912938cde5cf567ffb032c648a2f
jalexw <jalexwhitman@gmail.com>
2026-03-24 14:27:19 -0300

    e2e-auth-tests:0.2.11- confirm that the example resoruce server homepage is actually loaded correctly

--- tests/e2e-auth-tests/cypress/e2e/example_resource_server/ExampleResourceServer.cy.ts
+++ tests/e2e-auth-tests/cypress/e2e/example_resource_server/ExampleResourceServer.cy.ts
@@ -1,5 +1,7 @@
 describe("ExampleResourceServer", () => {
   it("can visit the example resource server", async () => {
     cy.visit("http://example-nextjs-resource-server:3007");
+    cy.url().should("include", "example-nextjs-resource-server");
+    cy.contains("h1", "@schemavaults/example-nextjs-resource-server");
   });
 });
