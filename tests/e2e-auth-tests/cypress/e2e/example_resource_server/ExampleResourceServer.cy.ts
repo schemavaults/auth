ca60794a01e13fc1ea3d781748ae6ccfc52f3209
jalexw <jalexwhitman@gmail.com>
2026-03-24 13:58:54 -0300

    use it instead of test

--- tests/e2e-auth-tests/cypress/e2e/example_resource_server/ExampleResourceServer.cy.ts
+++ tests/e2e-auth-tests/cypress/e2e/example_resource_server/ExampleResourceServer.cy.ts
@@ -1,5 +1,5 @@
 describe("ExampleResourceServer", () => {
-  test("can visit the example resource server", async () => {
+  it("can visit the example resource server", async () => {
     cy.visit("http://example-nextjs-resource-server:3007");
   });
 });
