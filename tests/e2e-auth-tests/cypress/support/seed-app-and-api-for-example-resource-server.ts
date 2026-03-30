5bb83ccc4bd7217a6eb1bf3e66bf3c7f0bc59ee5
jalexw <jalexwhitman@gmail.com>
2026-03-30 12:05:49 -0300

    e2e-auth-tests:0.2.17 - validate public key format

--- tests/e2e-auth-tests/cypress/support/seed-app-and-api-for-example-resource-server.ts
+++ tests/e2e-auth-tests/cypress/support/seed-app-and-api-for-example-resource-server.ts
@@ -1,3 +1,5 @@
+import { PEMFormat } from "@schemavaults/jwt";
+
 export async function seedAppAndApiForExampleResourceServer(
   auth_server_url: string,
   new_api_id: string,
@@ -6,6 +8,12 @@ export async function seedAppAndApiForExampleResourceServer(
 ): Promise<void> {
   const headers = new Headers();
   headers.set("Content-Type", "application/json");
+
+  if (!PEMFormat.isPemFormat(jwks_access_public_key, "PUBLIC")) {
+    throw new TypeError(
+      "Expected 'jwks_access_public_key' to be a valid public key in PEM format",
+    );
+  }
 
   const response = await fetch(
     `${auth_server_url}/api/test/seed/create-test-nextjs-app/${new_api_id}`,
