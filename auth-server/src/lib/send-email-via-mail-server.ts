362a28da3382b8aefd98f0d2b90ee01b7d3ecd29
jalexw <jalexwhitman@gmail.com>
2026-03-31 18:08:46 -0300

    auth-server:0.21.1 - types for send-email-via-mail-server.ts, using @schemavaults/send-email-api-options

--- auth-server/src/lib/send-email-via-mail-server.ts
+++ auth-server/src/lib/send-email-via-mail-server.ts
@@ -1,5 +1,6 @@
 import "server-only";
 import { getAppEnvironment, getHardcodedApiServerDomain, SCHEMAVAULTS_MAIL_SERVER, type SchemaVaultsApiServerDomainRef } from "@schemavaults/app-definitions";
+import { sendEmailRequestBodySchema, type SendEmailRequestBody } from "@schemavaults/send-email-api-options"
 
 function getDefaultMailServerUrl(): string {
   const hardcodedApiServerDomain: SchemaVaultsApiServerDomainRef = getHardcodedApiServerDomain(SCHEMAVAULTS_MAIL_SERVER.api_server_id, getAppEnvironment());
@@ -7,16 +8,24 @@ function getDefaultMailServerUrl(): string {
 }
 
 export async function sendEmailViaMailServer(
-  email_options: object,
+  email_options: SendEmailRequestBody,
   mail_server_access_token: string
 ): Promise<void> {
+  const mail_server_url: string = getDefaultMailServerUrl()
+
+  const parsed_email_options = await sendEmailRequestBodySchema.safeParseAsync(email_options)
+  if (!parsed_email_options.success) {
+    console.error("Invalid options to send email with: ", parsed_email_options.error);
+    throw new TypeError("Invalid options to send email with!")
+  }
+
   const headers = new Headers();
 
   headers.set("Content-Type", 'application/json');
-  headers.set(`Authorization`, `Bearer: ${mail_server_access_token}`)
+  headers.set(`Authorization`, `Bearer ${mail_server_access_token}`)
 
   const response = await fetch(
-    `${getDefaultMailServerUrl()}/api/send`,
+    `${mail_server_url}/api/send`,
     {
       method: "POST",
       headers,
