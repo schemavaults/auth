f66f78370b845b5d6f3135e8ff3457ce184496b5
jalexw <jalexwhitman@gmail.com>
2026-04-02 17:39:39 -0300

    auth-common:0.10.1 - organization-membership-role-type, organizations as entrypoint

---
+++ packages/auth-common/src/organizations/organization-membership-role-type.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const organizationMembershipRoleTypes = [
+  "owner",
+  "admin", // For virtual admin memberships in the schemavaults org
+  "member",
+] as const satisfies readonly string[];
+
+export type OrganizationMembershipRoleType =
+  (typeof organizationMembershipRoleTypes)[number];
+
+export const organizationMembershipRoleTypeSchema = z.string().refine(
+  (
+    maybeValidRole: string,
+  ): maybeValidRole is OrganizationMembershipRoleType => {
+    const validRoles: readonly string[] = organizationMembershipRoleTypes;
+    if (validRoles.includes(maybeValidRole)) {
+      return true;
+    }
+    return false;
+  },
+  `Invalid organization membership role! Should be one of: ${organizationMembershipRoleTypes.map((s) => `'${s}'`).join(", ")}`,
+);
+
+export function isValidOrganizationMembershipRoleType(
+  role: string,
+): role is OrganizationMembershipRoleType {
+  if (typeof role !== "string") return false;
+  return organizationMembershipRoleTypeSchema.safeParse(role).success;
+}
