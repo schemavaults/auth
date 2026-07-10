8563363197e5bd3608e721bd02335afb66f8a9b5
jalexw <jalexwhitman@gmail.com>
2026-07-10 09:01:11 -0400

    auth-server:0.32.4 - cleaning up spoofSuperuserAccessToken, only call getAppEnvironment() once

--- auth-server/src/lib/spoofSuperuserAccessToken.ts
+++ auth-server/src/lib/spoofSuperuserAccessToken.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
-import { type ApiServerId, type AppId, getAppEnvironment, getTokenAudienceForApiServerId } from "@schemavaults/app-definitions";
-import { JWT_Factory } from "@schemavaults/jwt";
+import { type ApiServerId, type AppId, getAppEnvironment, getTokenAudienceForApiServerId, SchemaVaultsAppEnvironment } from "@schemavaults/app-definitions";
+import { type I_JWT_Keys, JWT_Factory } from "@schemavaults/jwt";
 import { AuthServerJwtKeysManager } from "@/lib/AuthServerJwtKeysManager";
 import type { Kysely } from "@schemavaults/dbh";
 import type { AuthDatabase } from "@/lib/auth-db/auth-database-types";
@@ -11,19 +11,28 @@ import { getAuthServerOwnerOrganizationId } from "@/lib/config/auth-server-owner
 export interface ISpoofSuperuserTokenOpts {
   client_app_id: AppId;
   audience_id: ApiServerId;
-  db: Kysely<AuthDatabase>
+  db: Kysely<AuthDatabase>;
+  environment?: SchemaVaultsAppEnvironment;
 }
 
 const fakeSuperuserUid = "00000000-0000-0000-0000-000000000000" as const;
 
 export default async function spoofSuperuserAccessToken(
-  { client_app_id, audience_id, db }: ISpoofSuperuserTokenOpts
+  {
+    client_app_id,
+    audience_id,
+    db,
+    environment = getAppEnvironment()
+ }: ISpoofSuperuserTokenOpts
 ): Promise<AccessToken> {
-  const jwt_keys = await new AuthServerJwtKeysManager(db).getFreshEnoughKeysetOrCreateNew(audience_id)
+  const keys_manager = new AuthServerJwtKeysManager(db);
+  const jwt_keys: I_JWT_Keys = await keys_manager.getFreshEnoughKeysetOrCreateNew(
+    audience_id
+  );
 
   const jwt_factory = new JWT_Factory({
     client_app_id,
-    environment: getAppEnvironment(),
+    environment,
     jwt_keys,
     user: {
       email: "admin@schemavaults.com",
@@ -38,7 +47,7 @@ export default async function spoofSuperuserAccessToken(
   });
 
   const new_spoofed_access_token: AccessToken = await jwt_factory.access(
-    getTokenAudienceForApiServerId(audience_id, getAppEnvironment()),
+    getTokenAudienceForApiServerId(audience_id, environment),
   );
 
   return new_spoofed_access_token;
